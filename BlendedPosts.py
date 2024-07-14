af7ba89c54cd8107c411490c817c3c14d82ec423
unknown <rohansinghchauhan7761@gmail.com>
2024-07-14 21:41:24 +0530

    Similar Users

--- BlendedPosts.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.neighbors import NearestNeighbors
-
-users_df = pd.read_csv('RecommendationAlgo\users_100k_with_relationships.csv')
-posts_df = pd.read_csv('posts_100k_with_relationships.csv')
-interactions_df = pd.read_csv('interactions_100k_with_relationships.csv')
